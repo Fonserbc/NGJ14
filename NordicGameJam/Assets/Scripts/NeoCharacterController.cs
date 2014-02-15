b3d4226851b4dac66b1615d79056ac453e00b6e4
Vispilia <sknakjacobsen@gmail.com>
2014-02-15 01:43:52 +0100

    start stuff
    basic things

---
+++ NordicGameJam/Assets/Scripts/NeoCharacterController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeoCharacterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        #if UNITY_EDITOR
+        #endif
+
+        #if UNITY_ANDROID
+        if (Input.touchCount > 0)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                Vector3 worldSpaceHitPoint = hitInfo.point;
+            }
+        }
+        #endif
+        }
+}
